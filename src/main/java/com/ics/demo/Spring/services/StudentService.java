bacb625a5a35bcf5a0e52ae90a3ead59797a8ccb
COdiwuor <cyrilondime@gmail.com>
2019-09-19 09:20:18 +0300

    Update service

--- src/main/java/com/ics/demo/Spring/services/StudentService.java
+++ src/main/java/com/ics/demo/Spring/services/StudentService.java
@@ -7,8 +7,8 @@ import java.util.List;
 public interface StudentService {
     List<Student> findAll();
     Student findById(Long id);
-
     void delete(Long id);
+
     Student createStudent(Student student);
     Student update(Student student);
     Student update(Long id, Student student);
--- src/main/java/com/ics/demo/Spring/services/UniversityService.java
+++ src/main/java/com/ics/demo/Spring/services/UniversityService.java
@@ -7,16 +7,11 @@ import java.util.List;
 
 public interface UniversityService  {
     List<University> findAll();
-
     University findById(Long id);
-
     void delete(Long id);
 
     University createUniversity(University university);
-
     University update(University university);
-
     University update(Long id, University university); //overloading in OOP
-
     Student createStudent(Long id, Student student);
 }
