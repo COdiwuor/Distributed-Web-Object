5a204442d3914dc3a6a64a365d8f7141ad0d3e5f
COdiwuor <cyrilondime@gmail.com>
2019-09-19 13:02:10 +0300

    update test,demoapplication,feignrestclient

--- src/main/java/com/ics/demo/Spring/Test.java
+++ src/main/java/com/ics/demo/Spring/Test.java
@@ -1,5 +1,6 @@
 package com.ics.demo.Spring;
 
+import com.ics.demo.FeignRestClient;
 import com.ics.demo.Spring.models.University;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.ParameterizedTypeReference;
@@ -12,34 +13,54 @@ import java.util.List;
 
 @Component
 public class Test implements CommandLineRunner {
+    private final FeignRestClient feignRestClient;// Declare a feign client
+
+    public Test(FeignRestClient feignRestClient) {
+        this.feignRestClient = feignRestClient;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-        RestTemplate restTemplate = new RestTemplate();
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        ResponseEntity<List<University>>response= restTemplate.exchange(
+//                "http://10.51.10.111:9090/universities",
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<List<University>>() {
+//
+//                }
+//
+//
+//        );
+//
+//        List<University> universities = response.getBody();
+//        System.out.println(universities);
+//
+//        universities=feignRestClient.getAllUniversities();
+//        System.out.println("Feign Universities:"+universities.toString());
+//
+//
+//        University university = restTemplate.getForObject(
+//                "http://10.51.10.111:9090/universities/5",
+//                University.class
+//
+//        );
+//
+//        String url= "http://10.51.10.111:9090/universities/search?name="+university.getName();
+//        University searched = restTemplate.getForObject(
+//          url,
+//          University.class
+//        );
 
-        ResponseEntity<List<University>>response= restTemplate.exchange(
-                "http://10.51.10.111:9090/universities",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<University>>() {
-
-                }
+        //create a university of feign client
+        University createdUniversity = feignRestClient.createUniversity(new University("Kabarak university","Nakuru"));
+        System.out.println("Created University:"+createdUniversity.toString());
 
 
-        );
-
-        List<University> universities = response.getBody();
-        System.out.println(universities);
-
-        University university = restTemplate.getForObject(
-                "http://10.51.10.111:9090/universities/5",
-                University.class
-
-        );
-
-        String url= "http://10.51.10.111:9090/universities/search?name="+university.getName();
-        University searched = restTemplate.getForObject(
-          url,
-          University.class
-        );
+        //Updating a university of feign client
+        University updateUniversity = new University("Cyril's University","Kenya");
+        updateUniversity=feignRestClient.update(createdUniversity.getId(),updateUniversity);
+        System.out.println("Updated my university"+updateUniversity.toString());
     }
 }
